0f44c12dcdc2d06ab708485b95e7ce91139be3fc
Louis <brousselouis@hotmail.com>
2023-12-06 15:39:30 +0100

    Modifications 1

---
+++ app.js
@@ -0,0 +1,33 @@
+
+import express from 'express'
+import axios from 'axios' //exigé car ES6 (initialisation)
+const app = express()
+
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', function(request, response) {
+    response.render('form') 
+})
+
+
+app.post('/result', async function(request, response) { 
+    const zipcode = request.body.zipcode; 
+    const resp = await axios.get(`https://geo.api.gouv.fr/communes?codePostal=${zipcode}`);
+    const result = resp.data;
+    response.render('result', {result});
+ })
+
+app.get('/form', function(request, response) {
+    response.render('form') 
+})
+
+ 
+app.listen(3000, function() {
+   console.log('listening to port 3000')
+})
+
+
